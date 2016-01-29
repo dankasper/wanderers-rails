656d85c78c6d08f6aecd5eb56f5ac3acd0dc9944
Daniel Kasper <dankasper@gmail.com>
2016-01-29 06:13:44 +0000

    Make map cover whole screen, with translucent header on top

--- intentional-wanderers/app/assets/javascripts/map.js
+++ intentional-wanderers/app/assets/javascripts/map.js
@@ -1,13 +1,12 @@
 $(document).ready(function() {
-
-var map = L.map('map').setView([51.505, -0.09], 13);
-var StamenWatercolor = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
-    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
-    subdomains: 'abcd',
-    minZoom: 1,
-    maxZoom: 16,
-    ext: 'png'
-});
-StamenWatercolor.addTo(map);
-
+    var map = L.map('map').setView([51.505, -0.09], 13);
+    var StamenWatercolor = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        subdomains: 'abcd',
+        minZoom: 1,
+        maxZoom: 16,
+        ext: 'png'
+    });
+    StamenWatercolor.addTo(map);
+    map.zoomControl.setPosition('bottomleft');
 });
