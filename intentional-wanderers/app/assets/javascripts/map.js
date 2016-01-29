b9be8a2003c4f99277e67adf867c4ad16a983bb0
Daniel Kasper <dankasper@gmail.com>
2016-01-29 23:45:45 +0000

    Fix map initialization to occur on all page loads, even if the javascript is not reloaded

--- intentional-wanderers/app/assets/javascripts/map.js
+++ intentional-wanderers/app/assets/javascripts/map.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+var initMap = function() {
     var map = L.map('map').setView([51.505, -0.09], 13);
     var StamenWatercolor = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
         attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
@@ -9,4 +9,7 @@ $(document).ready(function() {
     });
     StamenWatercolor.addTo(map);
     map.zoomControl.setPosition('bottomleft');
-});
+};
+
+$(document).ready(initMap);
+$(document).on('page:load', initMap);
