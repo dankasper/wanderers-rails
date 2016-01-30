595cd70e39f1f050986fcb5aa4f0b01201466cee
Daniel Kasper <dankasper@gmail.com>
2016-01-30 04:31:18 +0000

    Initialize map centered on most recent location

--- intentional-wanderers/app/assets/javascripts/map.js
+++ intentional-wanderers/app/assets/javascripts/map.js
@@ -1,5 +1,7 @@
 var initMap = function() {
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    var lat = $('#map').data('initial-lat');
+    var lng = $('#map').data('initial-lng');
+    var map = L.map('map').setView([lat, lng], 11);
     var StamenWatercolor = L.tileLayer('http://stamen-tiles-{s}.a.ssl.fastly.net/watercolor/{z}/{x}/{y}.{ext}', {
         attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
         subdomains: 'abcd',
--- intentional-wanderers/app/controllers/welcome_controller.rb
+++ intentional-wanderers/app/controllers/welcome_controller.rb
@@ -1,2 +1,5 @@
 class WelcomeController < ApplicationController
+  def index
+    @current_location = Location.last
+  end
 end
