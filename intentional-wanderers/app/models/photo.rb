9a1643cf174ca35f668d7e01af7154b12e41887f
Daniel Kasper <dankasper@gmail.com>
2016-01-30 08:48:29 +0000

    Configure photo uploads to S3

--- intentional-wanderers/app/models/photo.rb
+++ intentional-wanderers/app/models/photo.rb
@@ -1,2 +1,9 @@
 class Photo < ActiveRecord::Base
+  dragonfly_accessor :image do
+    storage_options do |image|
+      {
+        path: "photos/#{SecureRandom.uuid}/#{image.name}"
+      }
+    end
+  end
 end
--- intentional-wanderers/db/schema.rb
+++ intentional-wanderers/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160130044442) do
+ActiveRecord::Schema.define(version: 20160130081539) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20160130044442) do
   create_table "photos", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "image_uid"
+    t.string   "image_name"
   end
 
   create_table "posts", force: :cascade do |t|
