4c2bb7ed532293814801623fd7324bc94b3e8ae3
Daniel Kasper <dankasper@gmail.com>
2016-01-31 07:48:32 +0000

    Display full posts on journal page and include positioned photos

--- intentional-wanderers/app/helpers/posts_helper.rb
+++ intentional-wanderers/app/helpers/posts_helper.rb
@@ -1,2 +1,13 @@
 module PostsHelper
+  def positioned_photo_tag photo_layout
+    content_tag :div, { class: 'positioned-photo', style: "padding-top: #{photo_layout.top}px; margin-bottom: -#{photo_layout.top}px;" } do
+      if photo_layout.align == 'clear'
+        content_tag :div, { style: 'float: left; width: 100%;' } do
+          image_tag photo_layout.photo.image.url, style: 'margin: auto'
+        end
+      else
+        image_tag photo_layout.photo.image.url, style: "float: #{photo_layout.align};"
+      end
+    end
+  end
 end
